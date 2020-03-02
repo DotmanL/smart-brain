4b9262bc87fcc14874f45a211b77b63b81cf44b4
DotmanL <60046089+DotmanL@users.noreply.github.com>
2020-03-02 14:38:42 +0100

    mobile responsive changes

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,18 +3,18 @@ import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageUrl, boxes }) => {
   return (
-    <div className='center ma'>
-      <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-        {boxes.map(box=>
-          <div key={`box${box.topRow}${box.rightCol}`}
-              className='bounding-box'
-              style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-}
+  <div className='image-box'>
+  <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
+  {boxes.map(box=>
+    <div key={`box${box.topRow}${box.rightCol}`}
+        className='face-box'
+        style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
+</div>
+  )}
+</div>
+);
+};
+
+   
 
 export default FaceRecognition;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,23 +4,23 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
-      <p className='f4 white'>
+    <p className='f6 white'>
       {'This Smart brain app will detect faces in your pictures. Give it a try '}
+      </p>
+      <p className='f6 white'>
+        {'You can make use of the link below or paste your own image url'}
       </p>
       <p className='f6 white'>
         {"https://www.sciencenewsforstudents.org/wp-content/uploads/2019/11/860_main_beauty.png"}
       </p>
-      <div className='center'>
-        <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' placeholder={'Enter image url'} type='text' onChange={onInputChange} />
-          <button
-            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
-            onClick={onButtonSubmit}
-          >Detect</button>
+    <div className='form'>
+        <div className='form-content'>
+            <input type="text" onChange={onInputChange} placeholder='Please enter an image URL'/>
+            <button className='pointer' onClick={onButtonSubmit}>Detect</button>
         </div>
-      </div>
     </div>
-  );
-}
-
+</div>
+);
+};
+   
 export default ImageLinkForm;
